7637aeb2bc9588f823a871d63652ae9abedfdf1b
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-03-02 02:02:17 +0300

    update

--- TsmartTechnicalInterviewAssignment.Api/Features/Users/Create/CreateUserCommand.cs
+++ TsmartTechnicalInterviewAssignment.Api/Features/Users/Create/CreateUserCommand.cs
@@ -1,7 +1,8 @@
-﻿using TsmartTechnicalInterviewAssignment.Shared;
+﻿using Microsoft.AspNetCore.Identity;
+using TsmartTechnicalInterviewAssignment.Shared;
 
 namespace TsmartTechnicalInterviewAssignment.Api.Features.Users.Create
 {
-    public record class CreateUserCommand(string UserName,string Email, string Password, ICollection<string> Roles) :IRequestByServiceResult<string>;
+    public record class CreateUserCommand(string UserName,string Email, string Password, ICollection<string> Roles) :IRequestByServiceResult<IdentityResult>;
     
 }
--- TsmartTechnicalInterviewAssignment.Api/Features/Users/Create/CreateUserCommandHandler.cs
+++ TsmartTechnicalInterviewAssignment.Api/Features/Users/Create/CreateUserCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Text;
 using TsmartTechnicalInterviewAssignment.Entities.Dtos;
 using TsmartTechnicalInterviewAssignment.Entities.Models;
 using TsmartTechnicalInterviewAssignment.Services.Contracts;
@@ -8,17 +11,30 @@ using TsmartTechnicalInterviewAssignment.Shared;
 
 namespace TsmartTechnicalInterviewAssignment.Api.Features.Users.Create
 {
-    public class CreateUserCommandHandler(IUserService userService, IMapper mapper) : IRequestHandler<CreateUserCommand, ServiceResult<string>>
+    public class CreateUserCommandHandler(IUserService userService, IMapper mapper) : IRequestHandler<CreateUserCommand, ServiceResult<IdentityResult>>
     {
-        public async Task<ServiceResult<string>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        public async Task<ServiceResult<IdentityResult>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
             var hasUser = await userService.FindByEmailAsync(request.Email);
             if (hasUser is not null)
-                return ServiceResult<string>.Error("Kullanıcı mevcut", "Kayıt olmak için farklı bir email adresi kullanın.", HttpStatusCode.BadRequest);
+                return ServiceResult<IdentityResult>.Error("Kullanıcı mevcut", "Kayıt olmak için farklı bir email adresi kullanın.", HttpStatusCode.BadRequest);
 
             var appUserAsDto = mapper.Map<AppUserRegisterDto>(request);
             var identityResult = await userService.RegisterUser(appUserAsDto);
-            return ServiceResult<string>.SuccessAsCreated(identityResult, $"/user/{identityResult}");
+            if (!identityResult.Succeeded)
+            {
+                var errors = new StringBuilder();
+                foreach (var item in identityResult.Errors)
+                    errors.Append(item.Description);
+                return ServiceResult<IdentityResult>.Error(new ProblemDetails()
+                {
+                    Detail = errors.ToString()
+
+                }, HttpStatusCode.BadRequest);
+
+            }
+             
+            return ServiceResult<IdentityResult>.SuccessAsCreated(identityResult, $"/user/{identityResult}");
         }
     }
 }
